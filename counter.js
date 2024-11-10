dba7b3b283da181caa423e6a86f514fdeda5027a
unknown <soloplayerno2706@gmail.com>
2024-11-10 20:34:18 +0700

    local storage

--- counter.js
+++ counter.js
@@ -1,13 +1,15 @@
-let counter = 0;
+if(!localStorage.getItem){
+    localStorage.setItem('counter',0);
+}
+let counter = localStorage.getItem('counter');
 function count(){
     counter++;
     document.querySelector('h1').innerHTML = counter;
     if(counter % 10 === 0){
         alert(`Counter is ${counter}`);
     }
+    localStorage.setItem('counter', counter);
 }
 document.addEventListener('DOMContentLoaded', function() {
-    document.querySelector('button').onclick = ()=>{
-        setInterval(count,1000);
-    };
+    document.querySelector('button').onclick = count;
 })
