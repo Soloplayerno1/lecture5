8cba7c271d6dacc1f41dc972cac46edf580a1419
unknown <soloplayerno2706@gmail.com>
2024-11-12 14:08:19 +0700

    moving( animationPlayState)

--- singlepage/singlepage/settings.py
+++ singlepage/singlepage/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'scroll',
     'singlepage1',
     'django.contrib.admin',
     'django.contrib.auth',
--- singlepage/singlepage/urls.py
+++ singlepage/singlepage/urls.py
@@ -21,5 +21,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("singlepage1.urls"))
+    path('', include("scroll.urls")),
 ]
